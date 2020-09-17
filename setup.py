b8f801031167849777d20ae28c2163b414b32ad3
DanilKorabelnikov <71430053+DanilKorabelnikov@users.noreply.github.com>
2020-09-17 11:05:12 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().splitlines()
+
+setuptools.setup(
+    name="aiovkdonate",
+    version="0.0.1",
+    author="DanilKorabelnikov",
+    description="",
+    url="https://github.com/DanilKorabelnikov/vkdonate",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=requirements
+)
