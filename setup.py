2d87bec282b52fa401c8054f2cc0e6fddf314676
DanilKorabelnikov <71430053+DanilKorabelnikov@users.noreply.github.com>
2020-10-20 19:21:14 +0300

    update

--- setup.py
+++ setup.py
@@ -1,19 +1,31 @@
 import setuptools
 
-with open("requirements.txt", "r") as f:
-    requirements = f.read().splitlines()
+try:
+    with open("README.md", "r", encoding="utf-8") as f:
+        long_description = f.read()
+except UnicodeDecodeError:
+    with open("README.md", "r") as f:
+        long_description = f.read()
+except FileNotFoundError:
+    long_description = ""
+
+with open("aiovkdonate/const/const.py") as f:
+    exec(f.read())
 
 setuptools.setup(
-    name="aiovkdonate",
-    version="0.0.2",
-    author="DanilKorabelnikov",
-    description="",
-    url="https://github.com/DanilKorabelnikov/vkdonate",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=requirements
+		name="aiovkdonate",
+		version=locals()["__version__"],
+		author=locals()["__author__"],
+		description="Async VKDonate API!",
+		long_description=long_description,
+		long_description_content_type="text/markdown",
+		license="MIT",
+		url="https://github.com/DanilKorabelnikov/vkdonate",
+		packages=setuptools.find_packages(),
+		classifiers=[
+			"Programming Language :: Python :: 3.6",
+			"Programming Language :: Python :: 3.7",
+			"Operating System :: OS Independent",
+		],
+		install_requires=["aiohttp", "pydantic"]
 )
